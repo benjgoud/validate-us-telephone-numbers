824030ef74e2eac82db3909d7be032f1ffc5c679
benjgoud <benj@goudreau.lt>
2018-05-16 19:45:58 -0600

    make it work

--- validate-us-telephone-numbers.js
+++ validate-us-telephone-numbers.js
@@ -1,6 +1,6 @@
 function telephoneCheck(str) {
 	// Good luck!
-	return str.replace(/\D/, '').length < 10 ||
-		str.replace(/\D/, '').length > 11 ? false :
-		/(?:1)?\d{3}\d{3}\d{4}/.test(str);
+	return str.replace(/\D/g, '').length < 10 ||
+		str.replace(/\D/g, '').length > 11 ? false :
+		/^(?:1 ?)?(?:\(\d{3}\)|\d{3})[ -]?\d{3}[ -]?\d{4}$/.test(str);
 }
